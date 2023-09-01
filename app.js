d9f6ac1fc0e5a3a02752aae7b5bc2117c1326204
jormanb <jormanb64@gmail.com>
2023-09-01 19:54:05 -0400

    Database added, v1.0 complete

--- app.js
+++ app.js
@@ -1,12 +1,22 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const ejs = require('ejs');
+const mongoose = require('mongoose');
+const { redirect } = require('express/lib/response');
 
 const app = express();
 app.use(express.static("public"));
 app.use(bodyParser.urlencoded({extended:true}));
 app.set('view engine', 'ejs');
 
+mongoose.connect('mongodb://localhost:27017/todolist', {UseNewUrlParser:true});
+
+const todolistSchema = new mongoose.Schema({
+    content: String,
+    _id: String
+})
+
+const task = new mongoose.model('Task', todolistSchema);
 
 let date = new Date();
 let param = {
@@ -15,41 +25,49 @@ let param = {
     month: "long",
 }
 let today = date.toLocaleDateString("es-ES", param);
-
-let itemList = [];
-
-function Erase (claves) {
-    let cont=0;
-    let keycont=0;
-    for (let i=0; i<itemList.length; i++) {
-        if (i == claves[keycont]) {
-            keycont++;
-        } else {
-            itemList[cont] = itemList[i];
-            cont++;
-        }
-    }
-    while(itemList.length > cont) {
-        itemList.pop();
-    }
-}
+let size = -1;
 
 app.get("/", (req, res)=>{
-    res.render('index', {Fecha: today, Lista: itemList});
+    task.find().then(itemList => {
+        size = itemList.length;
+        res.render('index', {Fecha: today, Lista: itemList});
+    }).catch(err => {
+        console.log(err);
+    })
 })
 
+function genID ( ) {
+    return parseInt((Math.random()*date.getMilliseconds())*10000000);
+}
 
 app.post("/add", (req, res)=>{
     let item = req.body.newItem;
     if (item != "") {
-        itemList.push(item);
+        task.create({content:item, _id: genID()}).then(query=>{res.redirect("/");})
+    } else {
         res.redirect("/");
     }
 })
 
+function del (list) {
+    if(list.length) {
+        __id = list[(list.length-1)];
+        list.pop();
+        task.deleteOne({_id:__id}).then(res => {
+            del(list);
+            return;
+        }).catch (err => {
+            console.log(err);
+        })
+    }
+    return;
+}
+
 app.post("/erase", (req, res)=>{
-    Erase(Object.keys(req.body));
-    res.redirect("/");
+    let id = Object.keys(req.body);
+    console.log(id);
+    del(id);
+    res.redirect('/');
 })
 
 app.listen(3000, ()=>{
